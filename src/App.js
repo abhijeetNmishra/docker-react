b1fc551a389db669555826cc13c0d3423abdb2ee
abhijeetNmishra <geminiabhijeet@gmail.com>
2024-01-02 17:16:41 -0600

    changed message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          Chnaging file to see if Docker image and container will be rebuild
         </a>
       </header>
     </div>
