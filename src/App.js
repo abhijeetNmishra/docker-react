3d67a3bcb05e0847c1a6af1c5ad30710c0f9ec4a
abhijeetNmishra <geminiabhijeet@gmail.com>
2024-01-03 20:45:04 -0600

    added changes to commited to feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react - Hello Abhijet Mishra
+          learn react - Hello Pragya Kumari
         </a>
       </header>
     </div>
